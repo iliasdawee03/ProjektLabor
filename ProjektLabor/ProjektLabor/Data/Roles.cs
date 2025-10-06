006c764085d990d65503a7b0f7f17f0d5fde3118
Bence Horváth <66383088+Benc313@users.noreply.github.com>
2025-10-06 20:42:14 +0200

    refactor: Update DbSet properties to virtual and change Company role to static

--- ProjektLabor/ProjektLabor/Data/Roles.cs
+++ ProjektLabor/ProjektLabor/Data/Roles.cs
@@ -3,7 +3,7 @@
     public class Roles
     {
         public string Admin { get; set; } = "Admin";
-        public string Company { get; set; } = "Company";
+        public static string Company { get; set; } = "Company";
         public string JobSeeker { get; set; } = "JobSeeker";
 
     }
